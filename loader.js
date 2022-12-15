b85f2e1005df365e98d2026da2a0a64152f3f61c
Dr Elec <105297987+Dr-Elec@users.noreply.github.com>
2022-12-15 22:44:26 +0300

    Update v0.0.1.0

---
+++ loader.js
@@ -0,0 +1,14 @@
+
+fetch("style/styles.json")
+    .then((response) => response.json())
+    .then((data) => data.forEach((cur)=> {
+            document.head.innerHTML += `<link rel="stylesheet" href="${cur}">`
+        }))
+
+window.onload = () => {
+    document.querySelectorAll("fetch").forEach((elem) => {
+        fetch(elem.getAttribute("src"))
+            .then((response) => response.text())
+            .then((txt) => elem.parentElement.innerHTML = txt)
+    })
+}
